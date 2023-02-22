99b6e7680872a13809e2074c69e1751d6a13113d
kiran2eficensit <kirankumarp@eficensit.com>
2023-02-22 09:00:43 +0530

    updated script.js

--- js/script.js
+++ js/script.js
@@ -2,43 +2,43 @@
 $(function () {
   //desktop icons click event
   $("#assetvision").click(function () {
-    window.location = '../assetvision.html';
+    window.location = '../innvendt/assetvision.html';
   });
   $("#earthe5").click(function () {
-    window.location = '../earthe5.html';
+    window.location = '../innvendt/earthe5.html';
   });
   $("#smartloto").click(function () {
-    window.location = '../smartloto.html';
+    window.location = '../innvendt/smartloto.html';
   });
   //asset tabs 
   $("#asset_tab_1").click(function () {
-    window.location = '../assetvision.html#feature_1';
+    window.location = '../innvendt/assetvision.html#feature_1';
   });
   $("#asset_tab_2").click(function () {
-    window.location = '../assetvision.html#feature_2';
+    window.location = '../innvendt/assetvision.html#feature_2';
   });
   $("#asset_tab_3").click(function () {
-    window.location = '../assetvision.html#feature_3';
+    window.location = '../innvendt/assetvision.html#feature_3';
   });
   //earthe5 tabs
   $("#earthe5_tab_1").click(function () {
-    window.location = '../earthe5.html#feature_1';
+    window.location = '../innvendt/earthe5.html#feature_1';
   });
   $("#earthe5_tab_2").click(function () {
-    window.location = '../earthe5.html#feature_2';
+    window.location = '../innvendt/earthe5.html#feature_2';
   });
   $("#earthe5_tab_3").click(function () {
-    window.location = '../earthe5.html#feature_3';
+    window.location = '../innvendt/earthe5.html#feature_3';
   });
   //smartloto tabs
   $("#smartloto_tab_1").click(function () {
-    window.location = '../smartloto.html#feature_1';
+    window.location = '../innvendt/smartloto.html#feature_1';
   });
   $("#smartloto_tab_2").click(function () {
-    window.location = '../smartloto.html#feature_2';
+    window.location = '../innvendt/smartloto.html#feature_2';
   });
   $("#smartloto_tab_3").click(function () {
-    window.location = '../smartloto.html#feature_3';
+    window.location = '../innvendt/smartloto.html#feature_3';
   });
   //mobile tabs
   
